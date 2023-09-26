9998a9f737a91e3107769fc9c94d23d1bd6cdb44
Gabriel  Bastos <57567691+bastosgabriel312@users.noreply.github.com>
2023-09-26 11:36:46 -0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,48 @@
+from utils import * 
+
+###### CONFIGURAÇÕES DA BINANCE
+api_key_binance = sua_chave
+api_secret_binance = sua_chave
+client = Client(api_key_binance, api_secret_binance)
+
+symbol = 'BTCUSDT'                                 ## MOEDA
+interval = Client.KLINE_INTERVAL_15MINUTE          ## INTERVALO
+limit = 10000                                       ## LIMITE
+
+###### RETORNA OS DADOS HISTÓRICOS COM MÉTRICA DE MVRV
+df = get_concatenate_klines_mvrv(client,symbol,interval,limit)
+
+###### PREPROCESSA OS DADOS
+dataset = preprocess_manual(df)
+
+COLUNAS_DATASET_COM_ALV0 = dataset.columns
+COLUNAS_DATASET_SEM_ALVO = dataset.copy().drop('target-high', axis=1).columns
+
+# Separando as variáveis independentes (X) e a variável alvo (y)
+dataset = dataset.copy()
+X = dataset.drop(['target-high'], axis=1)
+y = dataset['target-high']
+
+#REGRESSÃO LINEAR MULTIPLA (ALVO TARGET-HIGH)
+# Dividindo os dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Criando um objeto de regressão linear
+lr = LinearRegression()
+
+# Treinando o modelo com os dados de treinamento
+lr.fit(X_train, y_train)
+
+# Fazendo previsões com os dados de teste
+y_pred = lr.predict(X_test)
+
+# Avaliando o desempenho do modelo
+mse_lr = mean_squared_error(y_test, y_pred)
+r2_lr = r2_score(y_test, y_pred)
+
+print(f'mean_squared_error: {mse_lr}')
+print(f'r2 score: {r2_lr}')
+
+# Salvar um modelo treinado
+with open(f'/content/drive/MyDrive/1_experimento/modelo/rl_{symbol}.pkl', 'wb') as arquivo:
+    pickle.dump(lr, arquivo)
