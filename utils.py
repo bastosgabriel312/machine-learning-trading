4ddc4abdb7523e3cdd341452c4d4ea44eece67c2
Gabriel  Bastos <57567691+bastosgabriel312@users.noreply.github.com>
2023-09-26 11:48:25 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,93 +2,172 @@ import numpy as np
 import pandas as pd
 from binance.client import Client
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, Dropout
 from sklearn.metrics import precision_score, recall_score, f1_score
-from keras.optimizers import RMSprop, Adam, SGD 
+from keras.optimizers import RMSprop, Adam, SGD
 import csv
+from datetime import datetime
+import time
+import pickle
 
+# Função para calcular o ERRO MÉDIO ABSOLUTO
+def mae(y_true, predictions):
+    y_true, predictions = np.array(y_true).astype(float), np.array(predictions).astype(float)
+    return np.mean(np.abs(y_true - predictions))
 
-def retrieve_historical_data(api_key, api_secret, symbol, interval, limit):
-    client = Client(api_key, api_secret)
+# Função para calcular as métricas e imprimir
+def calculate_metrics(model, X, y):
+    y_pred = model.predict(X)
+    r2 = r2_score(y, y_pred)
+    scores = cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=5)
+    rmse_scores = np.sqrt(-scores)
+    mae_score = mae(y, y_pred)
+    return r2, mae_score, rmse_scores.mean()
+
+# Recupera os dados históricos da binance e retorna um dataframe
+def retrieve_historical_data_binance(client, symbol, interval, limit):
     klines = client.get_klines(symbol=symbol, interval=interval, limit=limit)
     df = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time',
                                        'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume',
                                        'taker_buy_quote_asset_volume', 'ignore'])
-    df.set_index('timestamp', inplace=True)
     df['close'] = df['close'].astype(float)
-    df = df.dropna()
-
     return df
 
 
-def preprocess_data(df):
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    df_scaled = scaler.fit_transform(df)
-    closing_price_scaler = MinMaxScaler(feature_range=(0, 1))
-    closing_price_scaler.fit_transform(df['close'].values.reshape(-1, 1))
-    X, y = create_dataset(df_scaled)
-    return X, y, scaler, closing_price_scaler
+def get_klines_df(client, symbol, interval, total_periods=2000):
+    klines_df = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
+
+    periods_received = 0
+    last_timestamp = None
+
+    while periods_received < total_periods:
+        klines = client.get_klines(
+            symbol=symbol,
+            interval=interval,
+            limit=total_periods - periods_received,
+            endTime=last_timestamp
+        )
+
+        if not klines:
+            break
+
+        for kline in klines:
+            klines_df.loc[len(klines_df)] = kline
+
+        periods_received += len(klines)
+
+        last_timestamp = klines[0][0]
+
+    # Convert timestamp column to datetime
+    klines_df['timestamp_to_date'] = pd.to_datetime(klines_df['timestamp'], unit='ms')
+
+    return klines_df.sort_values(by='timestamp')
 
 
-def create_dataset(data):
-    X, y = [], []
-    for i in range(60, len(data)):
-        X.append(data[i-60:i, :])
-        y.append(data[i, 0])
-    return np.array(X), np.array(y)
+# Calcula manualmente a métrica de mvrv_ratio
+def retrieve_mvrv_ratio(df_binance):
+    df_binance['mvrv_ratio'] = df_binance['close'].astype(float) / ((df_binance['close'].astype(float) + df_binance['high'].astype(float)+ df_binance['low'].astype(float) + df_binance['open'].astype(float)) / 4.0)
+    return df_binance
+
+# Retorna o datafrime da Binance com o mvrv calculado
+def get_concatenate_klines_mvrv(client, symbol, interval, limit):
+    df_binance_klines = get_klines_df(client, symbol, interval, limit)
+
+    #from_date,to_date = timestamp_to_datestring(df_binance_klines['timestamp'].iloc[-0]), timestamp_to_datestring(df_binance_klines['timestamp'].iloc[-1])
+    df_binance_klines['timestamp'] = df_binance_klines['timestamp'] // 1000
+    df_binance_klines = retrieve_mvrv_ratio(df_binance_klines)
+    return df_binance_klines
+
+# Função para calcular os quartis em uma coluna de DataFrame
+def calculate_quartiles(df, column_name):
+    """
+    Calcula os quartis (25%, 50%, 75%) em uma coluna de um DataFrame.
+
+    :param df: DataFrame
+    :param column_name: Nome da coluna
+    :return: Um dicionário com os valores dos quartis
+    """
+    if column_name not in df.columns:
+        raise ValueError(f"A coluna '{column_name}' não existe no DataFrame.")
+    df[column_name] = df[column_name].astype('float64')
+    values = df[column_name].values
+    first_quartile = np.percentile(values, 25)
+    second_quartile = np.percentile(values, 50)
+    third_quartile = np.percentile(values, 75)
+
+    return {
+        'first_quartile': first_quartile,
+        'second_quartile': second_quartile,
+        'third_quartile': third_quartile
+    }
 
 
+# Função para adicionar colunas indicando quartis a um DataFrame
+def add_quartile_columns(df, column_name):
+    """
+    Adiciona colunas ao DataFrame indicando se cada valor está no primeiro, segundo ou terceiro quartil.
 
+    :param df: DataFrame
+    :param column_name: Nome da coluna
+    :return: O DataFrame com as novas colunas
+    """
+    quartiles = calculate_quartiles(df, column_name)
+    first_quartile = quartiles['first_quartile']
+    second_quartile = quartiles['second_quartile']
+    third_quartile = quartiles['third_quartile']
+    df[f'{column_name}_in_first_quartile'] = (df[column_name] <= first_quartile).astype(int)
+    df[f'{column_name}_in_second_quartile'] = ((first_quartile < df[column_name]) & (df[column_name] <= second_quartile)).astype(int)
+    df[f'{column_name}_in_third_quartile'] = ((second_quartile < df[column_name]) & (df[column_name] <= third_quartile)).astype(int)
+    df.drop(column_name, axis=1)
+    return df
 
-def train_lstm_model(X_train, y_train, csv_filename):
-    model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
-    model.add(LSTM(units=50))
-    model.add(Dense(units=1))
-    optimizer = Adam(learning_rate=0.001)
-    model.compile(optimizer=optimizer, loss='mean_squared_error')
-    
-    loss_history = []  # Lista para armazenar a perda de cada época
-    
-    for epoch in range(5000):
-        history = model.fit(X_train, y_train, epochs=1, batch_size=32, verbose=0)
-        loss = history.history['loss'][0]
-        print(f"Epoch {epoch+1}/5000 - Loss: {loss}")
-        loss_history.append(loss)
-    # Salvar a perda em um arquivo CSV
-    with open(csv_filename, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Epoch', 'Loss'])
-        for epoch, loss in enumerate(loss_history):
-            writer.writerow([epoch+1, loss])
-    
-    return model
+# Função que transforma timestamp em data
+def timestamp_to_datestring(timestamp):
+    ts = timestamp/ 1000
+    data = datetime.fromtimestamp(ts)
+    return data.strftime('%Y-%m-%d')
 
+# Função utilizada para normalizar uma coluna
+def normalize_data(df, column_name):
+  # Inicialize o MinMaxScaler
+  scaler = MinMaxScaler()
+  # Ajuste o scaler aos dados
+  scaler.fit(df[[column_name]])
 
+  # Aplique a transformação aos dados
+  return scaler.transform(df[[column_name]])
 
-def predict_prices(model, X_test, closing_price_scaler):
-    predicted_price_scaled = model.predict(X_test)
-    predicted_price = closing_price_scaler.inverse_transform(predicted_price_scaled)
+# Função principal para pré processamento dos dados, utiliza as técnicas de quartile e normalização dos dados históricos
+def preprocess_manual(historical_data):
+  # adiciona a coluna que no dataset será o alvo
+  historical_data = calcular_target_high(historical_data.copy())
+  # Cria uma lista vazia para representar o df preprocessado
+  df_preprocessed = historical_data.copy()
 
-    return predicted_price
+  # Separa os parametros que serão normalizados e divididos em quartis
+  list_columns_quartil = ['volume','quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume','open']
+  list_columns_normalize = ['open','high','low','close','mvrv_ratio']
 
+  list_columns_normalize = ['volume','quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume','open','high','low','close','mvrv_ratio']
 
-def evaluate_predictions(actual_direction, predicted_direction):
-    precision = precision_score(actual_direction, predicted_direction, average='weighted', zero_division=1)
-    recall = recall_score(actual_direction, predicted_direction, average='weighted', zero_division=1)
-    f1 = f1_score(actual_direction, predicted_direction, average='weighted', zero_division=1)
+  for column in list_columns_normalize:
+    df_preprocessed[f'normalized_{column}'] = normalize_data(historical_data, column)
 
-    return precision, recall, f1
+  # Retira as colunas divididas em quartis
+  list_drop = ['ignore','timestamp','timestamp_to_date', 'close_time'] +list_columns_normalize +list_columns_quartil
+  df_preprocessed = df_preprocessed.drop(list_drop,axis=1)
 
+  return df_preprocessed
 
-def get_current_value(api_key, api_secret, symbol):
-    client = Client(api_key, api_secret)
-    klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, limit=1380)
-    last_minute_kline = klines[-2]
-    df_temp = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time',
-                                            'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume',
-                                            'taker_buy_quote_asset_volume', 'ignore'])
-    df_temp['close'] = df_temp['close'].astype(float)
-    df_temp.set_index('timestamp', inplace=True)
-    return df_temp
+def calcular_target_high(historical_data, num_periodos=4):
+    max_highs = []
+
+    for i in range(len(historical_data)):
+        # calcula o valor máximo da coluna 'high' nos próximos períodos.
+        max_high = historical_data['high'].iloc[i:i + num_periodos].max()
+        max_highs.append(max_high)
+
+    historical_data['target-high'] = max_highs
+    historical_data_alvo = historical_data.copy()
+    historical_data_alvo['target-high'] = historical_data_alvo['target-high'].astype('float64')
+    return historical_data_alvo
+
